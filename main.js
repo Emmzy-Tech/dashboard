78068c9f8dcd250a75e1eee76429d22559a8ddc5
emmaogit <emmao.github@gmail.com>
2022-05-24 09:44:47 +0100

    Toolkit Update

--- main.js
+++ main.js
@@ -8,3 +8,23 @@ menuBtn.addEventListener('click', function(){
 closeBtn.addEventListener('click', function(){
   sideMenu.style.display = 'none';
 })
+
+function copyTxt() {
+  /* Get the text field */
+  var copyText = document.getElementById("ref-txt");
+  var copied = document.querySelector('.copied');
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+  /* Alert the copied text */
+  copied.style.display = "block";
+  
+  setTimeout(function(){copied.style.display = "none"},3000);
+  
+}
+copied.innerHTML = "";
